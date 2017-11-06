62375679dc5b4382cbe2d4de3f8cfb2c0e98d54a
Annie <toussaint.am@gmail.com>
2017-11-06 18:44:15 -0500

    double checking

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SearchBar from './Components/SearchBar/SearchBar';
 import SearchResults from './Components/SearchResults/SearchResults';
 import PlayList from './Components/PlayList/PlayList';
+import Spotify from './Components/util/Spotify'; 
 
 class App extends Component {
   constructor(props) {
@@ -11,17 +12,43 @@ class App extends Component {
     this.state = {
        searchResults: [
         "name", "artist", "album"
+       ],
+       playlistName: '',
+       playlistTracks: [
+         "name", "artists", "album"
        ]
     };
+    this.addTrack = this.addTrack.bind(this); 
+    this.removeTrack = this.removeTrack.bind(this);
+    // this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
+addTrack(track) {
+  if(this.state.playlistTracks.indexOf(track) === -1) {
+    this.state.playlistTracks.push(track);
+    this.setState({ playlistTracks: this.state.playlistTracks });
+  }
+}
+
+  removeTrack(track) {
+   let tracks = this.state.playlistTracks;
+   tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+   this.setState({playlistTracks: tracks});
+}
+
+  search(term) {
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults});
+    });
+  } 
+  
   render() {
     return (
       <div className="App">
          <SearchBar />
         <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults} />
-          <PlayList />
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/> 
+          <PlayList playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onNameChange={this.updatePlaylistName}/>
         </div>
       </div>
     );
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -17,7 +17,7 @@ class PlayList extends React.Component {
             <div className="Playlist">
                 <input value="New Playlist" onChange={this.handleNameChange}  />
                 <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} />
-                <a class="Playlist-save">SAVE TO SPOTIFY</a>
+                <a className="Playlist-save">SAVE TO SPOTIFY</a>
             </div>
         );
     }
