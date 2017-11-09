341cdea4373499d6e64bd5940f4fa0d251b2bee8
Annie <toussaint.am@gmail.com>
2017-11-08 22:55:19 -0500

    submitting to advisor

--- src/App.js
+++ src/App.js
@@ -42,13 +42,13 @@ class App extends Component {
   }
 
   savePlaylist() {
-    const trackURIs = this.state.playlistTracks.map(track => track.uri);
-
-    Spotify.savePlaylist();
-
-    resetPlayList() {
-      this.state = this.searchResults;
-    }
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      });
+    });
   }
 
   search(term) {
